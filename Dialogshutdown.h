07baaeb50212df3a6aab88c583d5b81c8f82d0d8
noodleBin <hotdog156351@163.com>
2016-11-02 10:16:43 +0800

    ok

---
+++ Dialogshutdown.h
@@ -0,0 +1,29 @@
+#ifndef DIALOGSHUTDOWN_H
+#define DIALOGSHUTDOWN_H
+
+#include <QDialog>
+#include <QMovie>
+#include <QDir>
+
+namespace Ui {
+class DialogShutdown;
+}
+
+class DialogShutdown : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit DialogShutdown(QWidget *parent = 0);
+    ~DialogShutdown();
+    void setValue(quint8 value);
+
+private:
+    Ui::DialogShutdown *ui;
+    QString currentPath;
+    QMovie *movieshutdown;
+protected:
+    bool eventFilter(QObject *, QEvent *);
+};
+
+#endif // DIALOGSHUTDOWN_H
